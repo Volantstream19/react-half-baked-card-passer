cdd08271e4f311ba88880afc8c5dbab128be3283
King Kong <king-kong@gmail.com>
2023-01-25 12:49:14 -0800

    set the global state for to and from

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import './App.css';
 import initialCards from './cards-data';
 import Player from './components/Player';
 import CardList from './components/CardList';
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import ExecutePassButton from './components/ExecutePassButton';
+import { UserContext } from './context/CardContext.js';
 
 function App() {
   const [deck, setDeck] = useState(initialCards);
@@ -11,7 +12,7 @@ function App() {
   const [selectedCard, setSelectedCard] = useState();
   const [playerTwoHand, setPlayerTwoHand] = useState([]);
   const [playerThreeHand, setPlayerThreeHand] = useState([]);
-  const [from, setFrom] = useState('deck');
+  const [from, setFrom] = useContext(UserContext);
   const [to, setTo] = useState(1);
 
   function findCardIndex(value, suit, cards) {
@@ -44,51 +45,15 @@ function App() {
     <div className="App">
       <section>
         {/* if the player names are numbers, that will make our life easier later because we can reuse numbers as arrays. Note that this will make our app brittle! */}
-        <Player
-          to={to}
-          player={1}
-          hand={playerOneHand}
-          setFrom={setFrom}
-          selectedCard={selectedCard}
-          setTo={setTo}
-          setSelectedCard={setSelectedCard}
-        />
-        <Player
-          to={to}
-          player={2}
-          hand={playerTwoHand}
-          setFrom={setFrom}
-          selectedCard={selectedCard}
-          setTo={setTo}
-          setSelectedCard={setSelectedCard}
-        />
-        <Player
-          to={to}
-          player={3}
-          hand={playerThreeHand}
-          setFrom={setFrom}
-          selectedCard={selectedCard}
-          setTo={setTo}
-          setSelectedCard={setSelectedCard}
-        />
-        <CardList
-          cards={deck}
-          selectedCard={selectedCard}
-          setSelectedCard={setSelectedCard}
-          setFrom={setFrom}
-          player={'deck'}
-        />
+        <Player to={to} player={1} hand={playerOneHand} setFrom={setFrom} setTo={setTo} />
+        <Player to={to} player={2} hand={playerTwoHand} setFrom={setFrom} setTo={setTo} />
+        <Player to={to} player={3} hand={playerThreeHand} setFrom={setFrom} setTo={setTo} />
+
+        <CardList cards={deck} setFrom={setFrom} player={'deck'} />
       </section>
       <section>
         {selectedCard && (
-          <ExecutePassButton
-            passCard={passCard}
-            setFrom={setFrom}
-            from={from}
-            to={to}
-            selectedCard={selectedCard}
-            setSelectedCard={setSelectedCard}
-          />
+          <ExecutePassButton passCard={passCard} setFrom={setFrom} from={from} to={to} />
         )}
       </section>
     </div>
